62a5fb588fe12a1cb41b398fc17c975caeee8908
SPbryan007 <bryan.guns30@gmail.com>
2021-06-16 23:56:28 -0400

    first commit 2

--- backend/config/server.js
+++ backend/config/server.js
@@ -2,13 +2,12 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   admin: {
-    
     //url: '/', // Note: The administration will be accessible from the root of the domain (ex: http://yourfrontend.com/)
-    serveAdminPanel: true, // http://yourbackend.com will not serve any static admin files
+    serveAdminPanel: false, // http://yourbackend.com will not serve any static admin files
     autoOpen:false,
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'b2d76f12c3e7d6982d950fa72c929333'),
-    },
+    // auth: {
+    //   secret: env('ADMIN_JWT_SECRET', 'b2d76f12c3e7d6982d950fa72c929333'),
+    // },
   },
   
 });
